3e38fb452cfc167fe1dec416d26378e390ecf44c
well-pedro <eupedrowell@gmail.com>
2026-05-17 18:17:44 -0300

    feat/loanDto created

---
+++ server/src/dtos/loanDto.ts
@@ -0,0 +1,29 @@
+import { z } from "zod";
+
+export const createLoanSchema = z.object({
+  //checks if the ID is valid
+  id: z.uuid("O empréstimo deve conter um ID válido"),
+
+  //checks if the book ID is valid
+  idBook: z.uuid("O empréstimo deve conter um ID de livro válido"),
+
+  //checks if the client name is valid
+  clientName: z.string().min(1, "O empréstimo deve conter o nome do usuário"),
+
+  //checks if the borrow date is valid
+  //Date.parse converts a string to milliseconds, if it's not a number, the date is invalid
+  borrowDate: z.string().refine((date) => !isNaN(Date.parse(date)), {
+    message: "borrowDate deve ser uma data válida",
+  }),
+
+  //checks if the give back date is valid
+  //Date.parse converts a string to milliseconds, if it's not a number, the date is invalid
+  giveBackDate: z.string().refine((date) => !isNaN(Date.parse(date)), {
+    message: "giveBackDate deve ser uma data válida",
+  }),
+
+  //get the input status, if it is not provided, set it to "Em andamento"
+  status: z
+    .enum(["Em andamento", "Devolvido", "Atrasado"])
+    .default("Em andamento"),
+});
