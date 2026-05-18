17bc9a323e4c37c6e99514f7ce61882992056735
well-pedro <eupedrowell@gmail.com>
2026-05-18 12:47:31 -0300

    feat/added loanController

---
+++ server/src/controllers/loanController.ts
@@ -0,0 +1,76 @@
+import { Request, Response, NextFunction } from "express";
+import { createLoanSchema } from "../dtos/loanDto";
+import loanService from "@services/loanService";
+
+const loanController = {
+  //criando um novo emprestimo
+  async create(req: Request, res: Response, next: NextFunction) {
+    try {
+      //validando os dados que chegaram (req.body)
+      const validation = createLoanSchema.safeParse(req.body);
+
+      //se falhar, retorna 400 avisando do erro
+      if (!validation.success) {
+        return res.status(400).json({ error: "Dados invalidos" });
+      }
+
+      const newLoan = await loanService.create(validation.data);
+
+      return res.status(201).json(newLoan);
+    } catch (err) {
+      return next(err);
+    }
+  },
+
+  //buscando todos os emprestimos
+  async getAllLoans(req: Request, res: Response, next: NextFunction) {
+    try {
+      //espera o service fazer a busca no banco
+      const allLoans = await loanService.getAllLoans();
+
+      //retorna status 200 de ok com a lista inteira
+      return res.status(200).json(allLoans);
+    } catch (err) {
+      return next(err);
+    }
+  },
+
+  //buscando um emprestimo pelo id
+  async getLoanById(req: Request, res: Response, next: NextFunction) {
+    try {
+      const loan = await loanService.getLoanById(req.params.id);
+
+      return res.status(200).json(loan);
+    } catch (err) {
+      return next(err);
+    }
+  },
+
+  //buscando emprestimos pelo nome do cliente
+  async getLoansByClientName(req: Request, res: Response, next: NextFunction) {
+    try {
+      //pegando o clientName e buscando emprestimos
+      const loans = await loanService.getLoansByClientName(
+        req.params.clientName,
+      );
+
+      return res.status(200).json(loans);
+    } catch (err) {
+      return next(err);
+    }
+  },
+
+  //deletando um emprestimo pelo id
+  async deleteLoan(req: Request, res: Response, next: NextFunction) {
+    try {
+      //chama a funcao de deletar passando o id
+      await loanService.deleteLoan(req.params.id);
+
+      return res.status(204).send();
+    } catch (err) {
+      return next(err);
+    }
+  },
+};
+
+export default loanController;
