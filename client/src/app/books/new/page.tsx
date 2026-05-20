221aa08291ee88f23f9c4fd0a52260dd55dc1c0d
DgCavalcanti <diogo.naoseioq@gmail.com>
2026-05-20 16:26:37 -0300

    fix,style/ I finished the stylization and fixed some small bugs that I found, such as integer being accepted even when nothing was send,  added function to the button "Cancelar" and added the category cards

--- client/src/app/books/new/page.tsx
+++ client/src/app/books/new/page.tsx
@@ -5,8 +5,10 @@ export default function NewBook() {
   return (
     <div className="min-h-screen bg-gray-50">
       <div className="max-w-4xl mx-auto">
-        <h1 className="text-2xl font-bold text-gray-900">Cadastrar Novo Livro</h1>
-        <p className="text-sm text-gray-500 mt-1">Adicione um novo livro ao acervo</p>
+        <div className="mb-8 flex flex-col gap-1">
+          <h1 className="text-2xl font-bold text-gray-900">Cadastrar Novo Livro</h1>
+          <p className="text-sm text-gray-500 mt-1">Adicione um novo livro ao acervo</p>
+        </div>
         <RegisterBook />
       </div>
     </div>
--- client/src/components/book/RegisterBookForm.tsx
+++ client/src/components/book/RegisterBookForm.tsx
@@ -2,6 +2,7 @@
 import { z } from "zod";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
 
 export const RegisterBookSchema = z.object({
     title: z.string().min(1, "Este campo é obrigatório."),
@@ -17,12 +18,12 @@ export const RegisterBookSchema = z.object({
 
     publisher: z.string().min(1, "Este campo é obrigatório."),
 
-    year: z.coerce.number().int("Este campo é obrigatório."),
+    year: z.number("Este campo é obrigatório.").int(),
 
     totalQuantity: z
       .coerce
-      .number()
-      .int("Este campo é obrigatório.")
+      .number("Este campo é obrigatório.")
+      .int()
       .nonnegative(),
 
     category: z.enum([
@@ -37,6 +38,7 @@ export const RegisterBookSchema = z.object({
 export type RegisterBookFormData = z.infer<typeof RegisterBookSchema>;
 
 export default function RegisterBookForm() {
+  const router = useRouter();
   const{
     register,
     handleSubmit,
@@ -46,68 +48,119 @@ export default function RegisterBookForm() {
   } = useForm<RegisterBookFormData>({
     resolver: zodResolver(RegisterBookSchema),
   });
-
+  
   const onSubmit = (data: RegisterBookFormData) => {
     console.log(data);
   };
-
+  const categoriaSelecionada = watch("category");
   return (
     <div className="bg-white rounded-2xl shadow-sm border border-gray-100 p-6">
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-x-6 gap-y-4">
-          <label>
+          <div className="flex flex-col gap-1">
+            <label>
             Título
-          </label>
-          <input type="text" {...register("title")} className="border border-gray-300 p-2 rounded-md" />
-          {errors.title && (
-          <p className="text-red-500 text-sm mt-1">{errors.title.message}</p>
-          )}
-          <label>
-            Autor
-          </label>
-          <input type="text" {...register("author")} className="border border-gray-300 p-2 rounded-md" />
-          {errors.author && (
-            <p className="text-red-500 text-sm mt-1">{errors.author.message}</p>
-          )}
-          <label>
-            ISBN
-          </label>
-          <input type="text" {...register("isbn")} className="border border-gray-300 p-2 rounded-md" />
-          {errors.isbn && (
-          <p className="text-red-500 text-sm mt-1">*{errors.isbn.message}</p>
-          )}
-          <label>
-            Editora
-          </label>
-          <input type="text" {...register("publisher")} className="border border-gray-300 p-2 rounded-md" />
-          {errors.publisher && (
-            <p className="text-red-500 text-sm mt-1">*{errors.publisher.message}</p>
-          )}
-          <label>
-            Ano
-          </label>
-          <input type="text" {...register("year")} className="border border-gray-300 p-2 rounded-md"  />
-          {errors.year && (
-            <p className="text-red-500 text-sm mt-1">*{errors.year.message}</p>
-          )}
-          <label>
-            Quantidade Total
-          </label>
-          <input type="text" {...register("totalQuantity")} className="border border-gray-300 p-2 rounded-md" />
-          {errors.totalQuantity && (  
-          <p className="text-red-500 text-sm mt-1">*{errors.totalQuantity.message}</p>
-          )}
-          <label>
-            Categoria
-          </label>
-          <input type="text" {...register("category")} className="border border-gray-300 p-2 rounded-md"  />
-          {errors.category && (
-            <p className="text-red-500 text-sm mt-1">*{errors.category.message}</p>
-          )}
-          <div className="flex justify-end gap-3 border-t pt-4 mt-6"> 
-            <button type="button" className="bg-white border border-emerald-500 text-emerald-500 hover:bg-emerald-50">Cancelar</button>
-            <button type="submit" className="bg-emerald-500 hover:bg-emerald-600 text-white">Salvar Livro</button>
+            </label>
+            <input type="text" placeholder="Digite o título do livro" {...register("title")} className="border border-gray-300 p-2 rounded-md" />
+            {errors.title && (
+            <p className="text-red-500 text-sm mt-1">{errors.title.message}</p>
+            )}
           </div>
+          <div className="flex flex-col gap-1">
+            <label>
+              Autor
+            </label>
+            <input type="text" placeholder="Digite o nome do autor" {...register("author")} className="border border-gray-300 p-2 rounded-md" />
+            {errors.author && (
+              <p className="text-red-500 text-sm mt-1">{errors.author.message}</p>
+            )}
+          </div>
+          <div className="flex flex-col gap-1">
+            <label>
+              ISBN
+            </label>
+            <input type="text" placeholder="Digite o ISBN" {...register("isbn")} className="border border-gray-300 p-2 rounded-md" />
+            {errors.isbn && (
+            <p className="text-red-500 text-sm mt-1">*{errors.isbn.message}</p>
+            )}
+          </div>
+          <div className="flex flex-col gap-1">
+            <label>
+              Editora
+            </label>
+            <input type="text" placeholder="Digite a editora" {...register("publisher")} className="border border-gray-300 p-2 rounded-md" />
+            {errors.publisher && (
+              <p className="text-red-500 text-sm mt-1">*{errors.publisher.message}</p>
+            )}
+          </div>
+          <div className="flex flex-col gap-1">
+            <label>
+              Ano
+            </label>
+            <input type="text" placeholder="Digite o ano" {...register("year", { setValueAs: (value) => value === "" ? undefined : Number(value) })} className="border border-gray-300 p-2 rounded-md"  />
+            {errors.year && (
+              <p className="text-red-500 text-sm mt-1">*{errors.year.message}</p>
+            )}  
+          </div>
+          <div className="flex flex-col gap-1">
+            <label>
+              Quantidade Total
+            </label>
+            <input type="text" placeholder="Digite a quantidade" {...register("totalQuantity", { setValueAs: (value) => value === "" ? undefined : Number(value) })} className="border border-gray-300 p-2 rounded-md" />
+            {errors.totalQuantity && (  
+            <p className="text-red-500 text-sm mt-1">*{errors.totalQuantity.message}</p>
+            )}
+          </div>
+          <div className="md:col-span-2 flex flex-col gap-2 border-t pt pt-4 mt-6">
+            <label>Categoria</label>
+            <div className="grid grid-cols-2 md:grid-cols-5 gap-4">
+              <button 
+              type="button" 
+              onClick = {() => setValue("category", "Romance", {shouldValidate:true})} 
+              className={`h-24 rounded-xl border transition-colors ${
+              categoriaSelecionada === "Romance" 
+                ? "border-emerald-500 bg-emerald-50" 
+                : "border-gray-200 bg-white"}`}
+              >Romance</button>
+              <button type="button" 
+              onClick = {() => setValue("category", "Tecnologia", {shouldValidate:true})} 
+              className={`h-24 rounded-xl border transition-colors ${
+              categoriaSelecionada === "Tecnologia" 
+                ? "border-emerald-500 bg-emerald-50"
+                : "border-gray-200 bg-white"}`}
+              >Tecnologia</button>
+              <button 
+              type="button" 
+              onClick = {() => setValue("category", "História", {shouldValidate:true})} 
+              className={`h-24 rounded-xl border transition-colors ${
+              categoriaSelecionada === "História" 
+                ? "border-emerald-500 bg-emerald-50" 
+                : "border-gray-200 bg-white"}`} 
+              >História</button>
+              <button type="button" onClick = {() => setValue("category", "Ciências", {shouldValidate:true})} className={`h-24 rounded-xl border transition-colors ${
+              categoriaSelecionada === "Ciências" 
+                ? "border-emerald-500 bg-emerald-50" 
+                : "border-gray-200 bg-white"}`}
+              >Ciências</button>
+              <button type="button" onClick = {() => setValue("category", "Infantil", {shouldValidate:true})} className={`h-24 rounded-xl border transition-colors ${
+              categoriaSelecionada === "Infantil" 
+                ? "border-emerald-500 bg-emerald-50" 
+                : "border-gray-200 bg-white"}`}
+              >Infantil</button>
+              {errors.category && (
+              <p className="text-red-500 text-sm mt-1">*{errors.category.message}</p>
+              )}
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-end gap-3 border-t border-gray-200 pt-6 mt-8"> 
+          <button type="button" 
+          className="bg-white border border-emerald-500 text-emerald-500 hover:bg-emerald-50 px-6 py-2.5 rounded-lg font-medium text-sm transition-colors" 
+          onClick={() => router.push("/books")}
+          >Cancelar</button>
+          <button type="submit" 
+          className="bg-emerald-500 hover:bg-emerald-600 text-white px-6 py-2.5 rounded-lg font-medium text-sm transition-colors"
+          >Salvar Livro</button>
         </div>
       </form>
     </div>
