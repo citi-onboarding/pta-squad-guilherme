602c59d3e86881e99ad3da42972fdc1e64468c3c
Felipe <felipebelfort08@gmail.com>
2026-05-20 01:11:52 -0300

    feat: added BookCards and working searchbar and filter

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import Header from "@/components/header";
+import { cn } from "@/lib/utils";
 
 export default function RootLayout({
   children,
--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -1,5 +1,13 @@
 import LogoCITi from "./Logo.png";
+import RomanceBook from "./Romance.png";
+import TechnologyBook from "./Tecnologia.png";
+import ChildrenBook from "./Infantil.png";
+import HistoryBook from "./Historia.png";
+import ScienceBook from "./Ciencias.png";
 
 export { LogoCITi };
-
-
+export { RomanceBook };
+export { TechnologyBook };
+export { ChildrenBook };
+export { HistoryBook };
+export { ScienceBook };
