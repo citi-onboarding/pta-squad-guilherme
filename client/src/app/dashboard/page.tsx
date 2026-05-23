69b1b3aec99ead92a8cd506dc28fd77173b12124
well-pedro <eupedrowell@gmail.com>
2026-05-23 18:31:06 -0300

    fix/Adjusted widget proportions

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,11 +1,13 @@
+import LatestLoans from "@/components/dashboard/lastedLoans";
 import Widget from "../../components/Widget";
 import BooksChart from "../../components/dashboard/BooksByCategoryChart";
 
 export default function Dashboard() {
   return (
-    <div className="p-8 flex flex-col gap-8">
+    <div className="w-full max-w-[1450px] mx-auto p-6 flex flex-col gap-6">
       <Widget />
       <BooksChart />
+      <LatestLoans />
     </div>
   );
 }
