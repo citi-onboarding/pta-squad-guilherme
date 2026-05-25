5804722ad1126b015aa6fd1314d099a839f0023a
well-pedro <eupedrowell@gmail.com>
2026-05-25 20:22:49 -0300

    fix/Added page.tsx file

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -4,7 +4,7 @@ import LatestLoans from "../../components/dashboard/LatestLoans";
 
 export default function Dashboard() {
   return (
-    <div className="p-8 flex flex-col gap-8">
+    <div className="w-full max-w-[1450px] mx-auto p-6 flex flex-col gap-6">
       <Widget />
       <BooksChart />
       <LatestLoans />
