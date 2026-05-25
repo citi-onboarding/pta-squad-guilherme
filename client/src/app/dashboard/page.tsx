feb972f6a00baab378466acdfa8906f6ba06a147
well-pedro <eupedrowell@gmail.com>
2026-05-25 01:59:54 -0300

    feat/create LatestLoans table

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,11 +1,13 @@
 import Widget from "../../components/Widget";
 import BooksChart from "../../components/dashboard/BooksByCategoryChart";
+import LatestLoans from "../../components/dashboard/LatestLoans";
 
 export default function Dashboard() {
   return (
     <div className="p-8 flex flex-col gap-8">
       <Widget />
       <BooksChart />
+      <LatestLoans />
     </div>
   );
 }
