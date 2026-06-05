20e59701c7417e28157fea464ce98f5bd50150ff
Guilherme Valença Saunders <guilhermevalenca.s7@gmail.com>
2026-06-05 12:24:11 -0300

    feat: refactor loan service to include Loan interface and improve API methods

--- client/src/services/loan.ts
+++ client/src/services/loan.ts
@@ -1,6 +1,31 @@
 import api from "./api";
 
-export const getLoans = async () => {
-  const { data } = await api.get("/loans");
-  return data;
+export interface Loan {
+  id: string;
+  Name: string;
+  Email: string;
+  bookId: string;
+  dateBorrow: string;
+  dateGiveBack: string | null;
+  statusBook?: "EM_ANDAMENTO" | "DEVOLVIDO" | "ATRASADO";
+}
+
+export const createLoan = async (loan: Omit<Loan, "id">): Promise<Loan> => {
+  try{
+    const createNewLoan = await api.post("/loans", loan);
+    return createNewLoan.data;
+    }catch(error){
+        console.error("Erro ao chamar a API e criar empréstimo")
+        throw error;
+    };
+};
+
+export const getAllLoans = async (): Promise<Loan[]> => {
+  try{
+    const findManyLoans = await api.get("/loans");
+    return findManyLoans.data;
+  }catch(error){
+    console.error("Erro ao chamar a API e buscar empréstimos")
+    throw error;
+  };
 };
